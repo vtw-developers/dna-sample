cdc914d5dad6101f1f520fbe74dbf1c6baf76884
seungmin.bang <seungmin.bang@vtw.co.kr>
2022-05-30 16:01:16 +0900

    [EDIT] Edit init route

--- src/main/java/com/vtw/dna/DnaSampleApplication.java
+++ src/main/java/com/vtw/dna/DnaSampleApplication.java
@@ -1,7 +1,5 @@
 package com.vtw.dna;
 
-import com.vtw.dna.route.SampleRoute;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
